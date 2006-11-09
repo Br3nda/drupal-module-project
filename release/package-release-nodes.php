512a96e976bbf335f84978f117ac1a446a4229e3
dww <dww>
2006-11-09 09:56:47 +0000

    #92634: securing packaging script and other potential exploits by more rigorous validation of "cvs directory" and version format string: they can now only be alphanumeric with a few safe punctuation (e.g. "_-"). patch by dww from comment 3, with input from Heine, review by Kjartan.

--- release/package-release-nodes.php
+++ release/package-release-nodes.php
@@ -1,7 +1,7 @@
 #!/usr/local/bin/php
 <?php
 
-// $Id: package-release-nodes.php,v 1.1.2.9 2006/11/08 05:08:43 dww Exp $
+// $Id: package-release-nodes.php,v 1.1.2.10 2006/11/09 09:56:47 dww Exp $
 
 /**
  * @file
@@ -170,13 +170,13 @@ function package_releases($type) {
     $nid = $release->nid;
     $rev = ($tag == 'TRUNK' || $tag == 'HEAD') ? '-A' : "-r $tag";
     watchdog('release_package', t("Working on %type release: %id from $type: %tag", array('%type' => $release->rid == 1 ? t('core') : t('contrib'), '%id' => theme_placeholder($id), '%tag' => theme_placeholder($tag))));
-    $id = escapeshellcmd(filter_xss($id, array()));
-    $rev = escapeshellcmd(filter_xss($rev, array()));
+    $id = escapeshellcmd($id);
+    $rev = escapeshellcmd($rev);
     if ($release->rid == 1) {
       $built = package_release_core($nid, $id, $rev, $check_new);
     }
     else {
-      $dir = escapeshellcmd(filter_xss($release->directory, array()));
+      $dir = escapeshellcmd($release->directory);
       $built = package_release_contrib($nid, $id, $rev, $dir, $check_new);
     }
     if ($built) {
