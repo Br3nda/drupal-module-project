1540719bfebdf6b18d7af3e43c8db35bab736706
dww <dww>
2007-07-10 01:25:40 +0000

    #156550: Set appropriate Last-Modified HTTP header when serving release history .xml files to prevent trouble with caches.

--- release/project-release-serve-history.php
+++ release/project-release-serve-history.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Id: project-release-serve-history.php,v 1.1 2007/06/30 06:04:09 dww Exp $
+// $Id: project-release-serve-history.php,v 1.2 2007/07/10 01:25:40 dww Exp $
 
 /**
  * @file
@@ -29,7 +29,6 @@ define('HISTORY_ROOT', '');
 
 // Set page headers for the XML response.
 header('Content-Type: text/xml; charset=utf-8');
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 
 // Make sure we have the path arguments we need.
 $path = $_GET['q'];
@@ -65,7 +64,14 @@ if (!is_file($full_path)) {
   exit(1);
 }
 
+// Set the Last-Modified to the timestamp of the file, so that we properly
+// clear any HTTP caches once the file has been updated.
+$stat = stat($full_path);
+$mtime = $stat[9];
+header('Last-Modified: '. gmdate('D, d M Y H:i:s', $mtime) .' GMT');
+
 // Serve the contents.
+echo '<?xml version="1.0" encoding="utf-8"?>' ."\n";
 echo file_get_contents($full_path);
 
 
@@ -76,6 +82,7 @@ if (isset($_GET['sitekey'])) {
   }
 }
 */
+
 
 /**
  * Copy of core's check_plain() function.
@@ -88,6 +95,7 @@ function check_plain($text) {
  * Generate an error and exit.
  */
 function error($text) {
-  echo '<error>'. $text .'</error>';
+  echo '<?xml version="1.0" encoding="utf-8"?>'. "\n";
+  echo '<error>'. $text ."</error>\n";
   exit(1);
 }
