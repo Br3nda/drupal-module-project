1da437564d1310d8d86e9744641c09adb64c002e
dww <dww>
2009-03-08 02:59:32 +0000

    #394352 by pwolanin, dww: When a text keyword is present, the project browsing pages now sort by relevancy by default.

--- solr/ProjectSolrQuery.php
+++ solr/ProjectSolrQuery.php
@@ -5,8 +5,14 @@ include_once drupal_get_path('module', 'apachesolr') .'/Solr_Base_Query.php';
 class ProjectSolrQuery extends Solr_Base_Query {
   public $querypath = '';
 
-  function get_url_querystring() {
-    $querystring = parent::get_url_querystring();
+  function get_url_querystring($include_sort = TRUE) {
+    $querystring = '';
+    if ($fq = $this->rebuild_fq(TRUE)) {
+      $querystring = 'filters='. implode(' ', $fq);
+    }
+    if ($this->solrsort && $include_sort) {
+      $querystring .= ($querystring ? '&' : '') .'solrsort='. $this->solrsort;
+    }
     if ($this->querypath) {
       $querystring .= ($querystring ? '&' : '') .'text='. $this->querypath;
     }
