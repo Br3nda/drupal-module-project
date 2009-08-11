68eb41e984179c5c211dda2ce929655fb9b6813a
damz <damz>
2009-08-11 21:40:08 +0000

    Direct update of project_solr to match the new Apachesolr API. There are still a lot of issues with this, and the whole project search UI still needs to be refactored.

--- solr/ProjectSolrQuery.php
+++ solr/ProjectSolrQuery.php
@@ -3,19 +3,21 @@
 include_once drupal_get_path('module', 'apachesolr') .'/Solr_Base_Query.php';
 
 class ProjectSolrQuery extends Solr_Base_Query {
-  public $querypath = '';
-
-  function get_url_querystring($include_sort = TRUE) {
-    $querystring = '';
-    if ($fq = $this->rebuild_fq(TRUE)) {
-      $querystring = 'filters='. implode(' ', $fq);
+  function default_sorts() {
+    $sorts = array();
+    if (!empty($this->keys)) {
+      // The user has searched for a keyword.
+      $sorts['score'] = array('title' => t('Relevancy'), 'default' => 'asc');
     }
-    if ($this->solrsort && $include_sort) {
-      $querystring .= ($querystring ? '&' : '') .'solrsort='. $this->solrsort;
+    $sorts['sort_title'] = array('title' => t('Title'), 'default' => 'asc');
+    $sorts['created'] = array('title' => t('Creation date'), 'default' => 'desc');
+    if (module_exists('project_release')) {
+      $sorts['ds_project_latest_release'] = array('title' => t('Last release'), 'default' => 'desc');
+      $sorts['ds_project_latest_activity'] = array('title' => t('Recent activity'), 'default' => 'desc');
     }
-    if ($this->querypath) {
-      $querystring .= ($querystring ? '&' : '') .'text='. $this->querypath;
+    if (module_exists('project_usage')) {
+      $sorts['sis_project_release_usage'] = array('title' => t('Usage statistics'), 'default' => 'desc');
     }
-    return $querystring;
+    return $sorts;
   }
 }
