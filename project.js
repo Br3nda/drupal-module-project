b23a9b2dd8548d9c8ad858495037d97f029d6ea4
thehunmonkgroup <thehunmonkgroup>
2010-04-22 06:10:24 +0000

    #371957 by bdragon, csevb10, hunmonk, dww: Add customized per project type related vocabularies.

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-/* $Id: project.js,v 1.3 2009/01/12 20:07:19 dww Exp $ */
+/* $Id: project.js,v 1.4 2010/04/22 06:10:24 thehunmonkgroup Exp $ */
 
 Drupal.behaviors.projectAuto = function (context) {
   // The initially selected term, if any.
@@ -7,6 +7,7 @@ Drupal.behaviors.projectAuto = function (context) {
       if (this.checked) {
         tid = this.value;
       }
+      Drupal.projectMoveElement(this.value);
     })
     .click(function () {
       Drupal.projectSetTaxonomy(this.value);
@@ -14,20 +15,25 @@ Drupal.behaviors.projectAuto = function (context) {
   Drupal.projectSetTaxonomy(tid);
 }
 
+Drupal.projectMoveElement = function(tid) {
+  // move all elements with a class linked to this tid into the
+  // project taxonomy fieldset (similar to module sub-terms)
+  $('.related-tid-' + tid).each(function() {
+    $('#edit-tid-' + tid + '-wrapper').append($(this).parent().remove());
+  }); 
+}
+
 Drupal.projectSetTaxonomy = function (tid) {
   $('div.project-taxonomy-element select').each(function () {
-    // If this is the selector for the currently selected
-    // term, show it (in case it was previously hidden).
-    if (this.id == 'edit-tid-' + tid) {
+    // If this is the selector for the currently selected term or a
+    // related element, show it (in case it was previously hidden).
+    if (this.id == 'edit-tid-' + tid || $(this).hasClass('related-tid-' + tid)) {
       // Hide not the select but its containing div (which also contains
       // the label).
       $(this).parents('div.form-item').show();
     }
-    // Otherwise, empty it and hide it.
+    // Otherwise, hide it.
     else {
-      // In case terms were previously selected, unselect them.
-      // They are no longer valid.
-      this.selectedIndex = -1;
       $(this).parents('div.form-item').hide();
     }
   });
