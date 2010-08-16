fdaa56ceaba452fc986b6cab7de1eceac2bb36a1
dww <dww>
2010-08-16 17:57:34 +0000

    #880598 by csevb10: Fixed sorts on project navigation form.

--- solr/ProjectSolrQuery.php
+++ solr/ProjectSolrQuery.php
@@ -9,27 +9,4 @@ class ProjectSolrQuery extends Solr_Base_Query {
   public function get_path() {
     return $this->base_path;
   }
-
-  function default_sorts() {
-    $sorts = array();
-    if (!empty($this->keys)) {
-      // The user has searched for a keyword.
-      $sorts['score'] = array('title' => t('Relevancy'), 'default' => 'asc');
-    }
-    $sorts['sort_title'] = array('title' => t('Title'), 'default' => 'asc');
-    $sorts['created'] = array('title' => t('Creation date'), 'default' => 'desc');
-    if (module_exists('project_release')) {
-      $sorts['ds_project_latest_release'] = array('title' => t('Last release'), 'default' => 'desc');
-      $sorts['ds_project_latest_activity'] = array('title' => t('Recent activity'), 'default' => 'desc');
-      $active_terms = project_release_compatibility_list();
-      foreach ($active_terms as $tid => $term_name) {
-        $sorts['ds_project_latest_release_' . $tid] = array('title' => t('Last release'), 'default' => 'desc');
-        $sorts['ds_project_latest_activity_' . $tid] = array('title' => t('Recent activity'), 'default' => 'desc');
-      }
-    }
-    if (module_exists('project_usage')) {
-      $sorts['sis_project_release_usage'] = array('title' => t('Usage statistics'), 'default' => 'desc');
-    }
-    return $sorts;
-  }
 }
